bf77eefaa1f3dbb7e2e1d331d42a1a3b85b4aed0
Fares Ayman <faressaymann@gmail.com>
2026-04-15 11:09:08 +0200

    Add square component

---
+++ src/app/square/square.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  imports: [],
+  templateUrl: './square.html',
+  styleUrl: './square.scss',
+})
+export class Square {}
